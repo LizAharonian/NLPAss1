5ff4e99833f778792a8624855e80cd27e3f60590
LizAharonian <lizaharonian@gmail.com>
2018-11-04 20:00:58 +0200

    q_dict

--- MLETrain.py
+++ MLETrain.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 
 def main(argv):
     if (len(argv)!=3):
@@ -7,31 +8,57 @@ def main(argv):
     q_file = argv[1]
     e_file = argv[2]
 
-    e_mle_dict = {}
+    e_mle = Counter()
+    singles = Counter()
+    pairs = Counter()
+    triplets = Counter()
+
+    one_prev = None
+    two_prev = None
+
     with open(input_file_name, 'r') as input_file:
         content = input_file.readlines()
         for line in content:
-            words_list = line.strip('\n').strip().split(" ")
-            for word in words_list:
+            token_list = line.strip('\n').strip().split(" ")
+            for token in token_list:
                 try:
-                    k = word.rfind("/")
-                    w , tag = word[:k], word[k+1:]
-                    if (w,tag) in e_mle_dict:
-                        e_mle_dict[(w,tag)] +=1
-                    else:
-                        e_mle_dict[(w,tag)] = 1
+                    w , tag = get_word_and_tag(token)
+                    # fill e_mle
+                    e_mle[(w,tag)] +=1
+                    # fill q_mle
+                    singles[(tag,)] += 1
+                    if one_prev != None:
+                        pairs[(one_prev, tag)] += 1
+                    if two_prev != None:
+                        triplets[(two_prev, one_prev, tag)] += 1
+                    # swap and update params
+                    temp = one_prev
+                    one_prev = tag
+                    two_prev = temp
+
                 except Exception:
                     continue
-    write_e_dict_to_file(e_file, e_mle_dict)
+
+    write_q_mle_to_file(singles,pairs,triplets,q_file)
+    write_e_mle_to_file(e_mle,e_file)
+
+def write_q_mle_to_file(singles, pairs, triplets,q_file):
+    write_counter_to_file(q_file,singles,'w')
+    write_counter_to_file(q_file,pairs,'a')
+    write_counter_to_file(q_file,triplets,'a')
+
+def write_e_mle_to_file(e_mle, e_file):
+    write_counter_to_file(e_file,e_mle,'w')
+
+def write_counter_to_file(file_name, counter,flag):
+    with open(file_name, flag) as file:
+        for key in counter:
+            file.write(" ".join(key) + "\t" + str(counter[key]) + "\n")
 
 
-def write_e_dict_to_file(e_file, e_mle_dict):
-    with open(e_file, 'w') as file:
-        for key, value in e_mle_dict.iteritems():
-            word, tag = key
-            file.write(word + " " + tag + "\t" + str(value) + "\n")
-
-
+def get_word_and_tag(token):
+    k = token.rfind("/")
+    return token[:k], token[k + 1:]
 
 
 def getQ(t1,t2,t3):

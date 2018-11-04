a1119e81dd0096e0c65d76658d1b2abbdb7c1de1
LizAharonian <lizaharonian@gmail.com>
2018-11-04 17:34:53 +0200

    e_dict

--- MLETrain.py
+++ MLETrain.py
@@ -7,12 +7,35 @@ def main(argv):
     q_file = argv[1]
     e_file = argv[2]
 
+    e_mle_dict = {}
+    with open(input_file_name, 'r') as input_file:
+        content = input_file.readlines()
+        for line in content:
+            words_list = line.strip('\n').strip().split(" ")
+            for word in words_list:
+                try:
+                    k = word.rfind("/")
+                    w , tag = word[:k], word[k+1:]
+                    if (w,tag) in e_mle_dict:
+                        e_mle_dict[(w,tag)] +=1
+                    else:
+                        e_mle_dict[(w,tag)] = 1
+                except Exception:
+                    continue
+    write_e_dict_to_file(e_file, e_mle_dict)
+
+
+def write_e_dict_to_file(e_file, e_mle_dict):
+    with open(e_file, 'w') as file:
+        for key, value in e_mle_dict.iteritems():
+            word, tag = key
+            file.write(word + " " + tag + "\t" + str(value) + "\n")
+
 
 
 
 def getQ(t1,t2,t3):
     print "liz"
-
 
 if __name__ == "__main__":
 	main(sys.argv[1:])

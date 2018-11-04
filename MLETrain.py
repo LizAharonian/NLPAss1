e716be517d845afddd891c881348c5b210c0c08b
LizAharonian <lizaharonian@gmail.com>
2018-11-04 16:58:47 +0200

    Initial commit

---
+++ MLETrain.py
@@ -0,0 +1,20 @@
+import sys
+
+def main(argv):
+    if (len(argv)!=3):
+        exit(1)
+    input_file_name = argv[0]
+    q_file = argv[1]
+    e_file = argv[2]
+
+
+
+
+def getQ(t1,t2,t3):
+    print "liz"
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
+
+

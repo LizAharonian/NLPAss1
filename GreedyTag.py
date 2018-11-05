f652e8ca591d472577b392eba74190f4980e5397
OriBenZaken <orinetanel1@gmail.com>
2018-11-05 23:25:06 +0200

    skjsk

--- GreedyTag.py
+++ GreedyTag.py
@@ -1,6 +1,45 @@
 import sys
+import MLETrain as mle
 
 
+def get_top_tag(w,one_prev,two_prev):
+    max_score = 0
+    final_t = None
+    for t in mle.singles:
+        # calculate q
+        temp_score = mle.getE(w,t) * mle.getQ(t,one_prev,two_prev)
+        if temp_score > max_score:
+            max_score = temp_score
+            final_t = t
+    return final_t
+
+def greedy_algo():
+    output_file = open(out_file_name,'w')
+    with open(input_file_name, 'r') as input_file:
+        content = input_file.readlines()
+        for line in content:
+            words_and_tags = []
+            one_prev = None
+            two_prev = None
+            words_list = line.strip('\n').strip().split(" ")
+            for word in words_list:
+                try:
+                    tag = get_top_tag(word, one_prev, two_prev)
+                    words_and_tags.append((word,tag))
+                    # swap and update params
+                    temp = one_prev
+                    one_prev = tag
+                    two_prev = temp
+
+                except Exception:
+                    continue
+    output_file.close()
+
+
+def write_line_to_file(output_file, words_and_tags):
+    words_and_tags = ['/'.join(item) for item in words_and_tags]
+    line_to_write = " ".join(words_and_tags) + "\n"
+    output_file.write(line_to_write)
 
 
 argv = sys.argv[1:]
--- MLETrain.py
+++ MLETrain.py
@@ -1,10 +1,16 @@
 import sys
+import random
 from collections import Counter
 
 GAMA1 = 0.5
 GAMA2 = 0.25
 GAMA3 = 0.25
 
+e_mle = Counter()
+singles = Counter()
+pairs = Counter()
+triplets = Counter()
+words_counter = 0
 
 def write_q_mle_to_file(singles, pairs, triplets,q_file):
     write_counter_to_file(q_file,singles,'w')
@@ -28,64 +34,104 @@ def getE(w,t):
     if (singles[(t)]!=0):
         return e_mle[(w,t)]/singles[(t)]
     else:
-        return 0
+        #return 0
+        return random.uniform(0, 1)
 
 def getQ(t1,t2,t3):
-    return 5
+    return random.uniform(0, 1)
     #todo: q3 yoav
-    return GAMA1 * (triplets[(t1,t2,t3)]/ pairs[(t1,t2)]) + GAMA2 * (pairs[(t2,t3)]/singles[(t2,)]) + GAMA3 * (singles[(t3,)]/counter_words)
+    return GAMA1 * (triplets[(t1,t2,t3)]/ pairs[(t1,t2)]) + GAMA2 * (pairs[(t2,t3)]/singles[(t2,)]) + GAMA3 * (singles[(t3,)] / words_counter)
 
+def initialize_dicts_from_file(e_mle_file, q_mle_file):
+    global e_mle
+    e_mle = Counter()
+    global singles
+    singles = Counter()
+    global pairs
+    pairs = Counter()
+    global riplets
+    triplets = Counter()
+    # e_mle = Counter()
+    # singles = Counter()
+    # pairs = Counter()
+    # triplets = Counter()
 
-def liz():
-    print "kkk"
-
-    print ""
-argv = sys.argv[1:]
-if (len(argv)!=3):
-    exit(1)
-input_file_name = argv[0]
-q_file = argv[1]
-e_file = argv[2]
-
-e_mle = Counter()
-singles = Counter()
-pairs = Counter()
-triplets = Counter()
-
-one_prev = None
-two_prev = None
-
-with open(input_file_name, 'r') as input_file:
-    content = input_file.readlines()
-    counter_words = 0
-    for line in content:
-        token_list = line.strip('\n').strip().split(" ")
-        for token in token_list:
-            try:
-                w , tag = get_word_and_tag(token)
-                counter_words += 1
-                # fill e_mle
-                e_mle[(w,tag)] +=1
-                # fill q_mle
-                singles[(tag,)] += 1
-                if one_prev != None:
-                    pairs[(one_prev, tag)] += 1
-                if two_prev != None:
-                    triplets[(two_prev, one_prev, tag)] += 1
-                # swap and update params
-                temp = one_prev
-                one_prev = tag
-                two_prev = temp
-
-            except Exception:
+    with open(e_mle_file, 'r') as f:
+        content = f.readlines()
+        for line in content:
+            line = line.strip("\n")
+            if line == "":
                 continue
-        one_prev = None
-        two_prev = None
+            key, value = line.split("\t")
+            word, tag = key.split(" ")
+            e_mle[(word, tag)] = value
 
-write_q_mle_to_file(singles,pairs,triplets,q_file)
-write_e_mle_to_file(e_mle,e_file)
+    with open(q_mle_file, 'r') as f:
+        content = f.readlines()
+        for line in content:
+            line = line.strip("\n")
+            if line == "":
+                continue
+            key, value = line.split("\t")
+            tags = tuple(key.split(" "))
+            if len(tags) == 1:
+                singles[tags] = value
+            elif len(tags) == 2:
+                pairs[tags] = value
+            elif len(tags) == 3:
+                triplets[tags] = value
 
-# if __name__ == "__main__":
-# 	main(sys.argv[1:])
+def main(argv):
+    if (len(argv)!=3):
+        exit(1)
+    input_file_name = argv[0]
+    q_file = argv[1]
+    e_file = argv[2]
+
+    global e_mle
+    e_mle = Counter()
+    global singles
+    singles = Counter()
+    global pairs
+    pairs = Counter()
+    global riplets
+    triplets = Counter()
+
+    one_prev = None
+    two_prev = None
+
+    with open(input_file_name, 'r') as input_file:
+        content = input_file.readlines()
+        global lala
+        lala = 0
+        for line in content:
+            token_list = line.strip('\n').strip().split(" ")
+            for token in token_list:
+                try:
+                    w , tag = get_word_and_tag(token)
+                    lala += 1
+                    # fill e_mle
+                    e_mle[(w,tag)] +=1
+                    # fill q_mle
+                    singles[(tag,)] += 1
+                    if one_prev != None:
+                        pairs[(one_prev, tag)] += 1
+                    if two_prev != None:
+                        triplets[(two_prev, one_prev, tag)] += 1
+                    # swap and update params
+                    temp = one_prev
+                    one_prev = tag
+                    two_prev = temp
+
+                except Exception:
+                    continue
+            one_prev = None
+            two_prev = None
+
+    write_q_mle_to_file(singles,pairs,triplets,q_file)
+    write_e_mle_to_file(e_mle,e_file)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
 
 
